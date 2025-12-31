cd0624f34dd865413005dc272c951e5562bd625a
cadamsmith <cadamsmith.dev@gmail.com>
2025-12-31 11:58:40 -0500

    remove docs site for now

--- rollup.config.js
+++ rollup.config.js
@@ -24,7 +24,7 @@ const jsBuilds = [
     },
     plugins: [
       typescript({ compilerOptions: { outDir: 'dist', declaration: false } }),
-      copyToDocs(),
+      //copyToDocs(),
     ],
   },
   // DV Core: ESM
